55266393c764e2d5feebb0f89a74a07d84cac2b0
Alanvarghese-sbc <96782099+Alanvarghese-sbc@users.noreply.github.com>
2021-12-29 09:22:18 +0530

    Add files via upload

---
+++ Pattern lab 4.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main(void)
+{
+		int i,j,c=1;
+		for(i=5;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				printf("%d",c);
+			}
+			printf("\n");
+			c++;
+		}
+
+		return 0;
+	}
