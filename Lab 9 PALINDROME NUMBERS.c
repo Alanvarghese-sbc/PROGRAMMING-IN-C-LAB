4995f13646049575184f32c34ee01f31a4278fcd
Alan Varghese <96782099+Alanvarghese-sbc@users.noreply.github.com>
2021-12-30 23:31:49 +0530

    Add files via upload

---
+++ Lab 9 PALINDROME NUMBERS.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n,r,sum,temp;
+	printf("Enter the number : ");
+	fflush(stdout);
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		r=n%10;
+	    sum=sum*10+r;
+	    n=n/10;
+
+	}
+		if(temp==sum)
+		{
+			printf("%d is a PALINDROME NUMBER ",temp);
+		}
+		else
+		{
+		printf("%d is not a PALINDROME NUMBER",temp);
+		}
+	return 0;
+}
